585847ebf68c2775856b7e209e1d8157c5809b5e
msalina007 <79892093+msalina007@users.noreply.github.com>
2023-05-04 02:14:56 -0600

    added delete method to /comments route

--- server.js
+++ server.js
@@ -350,7 +350,7 @@ router.route('/comment')
     })
 
     .delete(authJwtController.isAuthenticated, function(req, res){
-        Comment.deleteOne({blogpostTitle: req.params['0']} && {username: this.username}, null, function(err, data){
+        Comment.deleteOne({blogpostTitle: req.body.title} && {username: this.username}, null, function(err, data){
             if(err){
                 return res.status(400).json(err);
         }
